55654c33ac804e31f31ba10b3fdfd8f7c6c3d135
Darshil Ladani <ladani.darshil511@gmsil.com>
2025-04-07 19:47:32 +0530

    adding user data

--- Deployment/User-data/script-app.sh
+++ Deployment/User-data/script-app.sh
@@ -9,5 +9,11 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y python3-pip
 
 # clonning repo
 git clone https://github.com/darshil511/APMC-price-predictor/
-pip install -r APMC-price-predictor/requirements.txt
 
+# create and start virtual env
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3-venv
+python3 -m venv APMC-price-predictor/.venv
+source APMC-price-predictor/.venv/bin/activate
+
+# install libraries
+pip install -r APMC-price-predictor/requirements.txt
