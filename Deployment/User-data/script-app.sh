e39375452b3bd4635797c15ff38e7e7b44947b38
Darshil Ladani <ladani.darshil511@gmsil.com>
2025-04-08 16:32:19 +0530

    Daily update

--- Deployment/User-data/script-app.sh
+++ Deployment/User-data/script-app.sh
@@ -2,10 +2,13 @@
 
 sudo apt update
 
-# install python
+# install python and postgres
 sudo DEBIAN_FRONTEND=noninteractive apt -y upgrade
 python3 -V
 sudo DEBIAN_FRONTEND=noninteractive apt install -y python3-pip
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libpq-dev -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install net-tools
+
 
 # clonning repo
 git clone https://github.com/darshil511/APMC-price-predictor/
@@ -17,3 +20,6 @@ source APMC-price-predictor/.venv/bin/activate
 
 # install libraries
 pip install -r APMC-price-predictor/requirements.txt
+
+
+# -> fill up.env and apmc-notifications-firebase-adminsdk-fbsvc-096b7561f5.json file
