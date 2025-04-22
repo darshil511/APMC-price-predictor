cb29d6e419492e720d018d79ca2fbcc4cc6205e4
Darshil Ladani <ladani.darshil511@gmsil.com>
2025-04-22 14:44:31 +0530

    added time_sleep of  44s �️so that droplets can poweroff before vpc terminates

--- Deployment/Terraform/vpc.tf
+++ Deployment/Terraform/vpc.tf
@@ -3,3 +3,10 @@ resource "digitalocean_vpc" "tfm-apmc-vpc"{
     region = var.region
     ip_range = "10.10.10.0/24"
 }
+resource "time_sleep" "wait_44_seconds_to_destroy" {
+  depends_on = [digitalocean_vpc.tfm-apmc-vpc]
+  destroy_duration = "44s"
+}
+resource "null_resource" "placeholder" {
+  depends_on = [time_sleep.wait_44_seconds_to_destroy]
+}
